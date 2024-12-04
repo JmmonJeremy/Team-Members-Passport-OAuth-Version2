52a6ae2c37c43ac5d432f3a63190050ce270495b
JmmonJeremy <suc19002@byui.edu>
2024-12-03 17:08:17 -0800

    Created swagger-doc to run from render site.

--- swagger.js
+++ swagger.js
@@ -6,7 +6,18 @@ const doc = {
     description: 'Computer Store API'
   },
   host: 'localhost:55000',
-  schemes: ['http']
+  schemes: ['http'],
+  securityDefinitions: {
+    GoogleOAuth2: {
+      type: 'oauth2',
+      flow: 'authorizationCode',
+      authorizationUrl: 'http://localhost:55000/auth/google',
+      tokenUrl: 'https://oauth2.googleapis.com/token',
+      clientId: process.env.GOOGLE_CLIENT_ID,  // Injecting from environment
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET, // Injecting from environment
+      description: 'Use Google OAuth2 to authenticate.',
+    },
+  },
 };
 
 const outputFile = './swagger.json';
